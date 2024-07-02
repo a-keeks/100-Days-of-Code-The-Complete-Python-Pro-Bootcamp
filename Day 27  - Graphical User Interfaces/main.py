7ad6403d4e9acf11abbe06911cbac0899719d78e
Akila Ambady <156318751+a-keeks@users.noreply.github.com>
2024-07-02 21:03:49 +0100

    Update main.py

--- Day 27  - Graphical User Interfaces/main.py
+++ Day 27  - Graphical User Interfaces/main.py
@@ -1,15 +1,37 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+def button_clicked():
+    my_label.config(text = get_content())
+
+def get_content():
+   #Get the content of Entry Widget
+   new_entry = entry.get()
+   return new_entry
+
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width = 500, height = 300)
+window.config(padx=100, pady=200)
 
 # Create Label
-my_label = tkinter.Label(text = "I am a Label", font = ("Calibri", 24, "bold"))
-my_label.pack() #puts label on to screen, default center screen
+my_label = Label(text = "I am a Label", font = ("Calibri", 24, "bold"))
+my_label.config(text = "New Text")
+my_label.grid(column=0, row=0) #puts label on to screen
+my_label.config(padx=50,pady=50)
 
+# Button
+button = Button(text = "Click Me", command = button_clicked)
+button.grid(column=1,row=1)
 
+# Entry
+entry = Entry()
+print(entry.get())
+entry.grid(column=4, row=3)
 
+# New Button
+new_button = Button(text = "New Button", command = button_clicked)
+new_button.grid(column=3,row=0)
 
 
 window.mainloop()
+
